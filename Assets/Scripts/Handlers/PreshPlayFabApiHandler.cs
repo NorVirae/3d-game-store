07bd3933be864c7388e496f3f89257ccbd686974
nkpresh <nkwuap@gmail.com>
2023-08-07 17:04:09 +0100

    Inventory data update

--- Assets/Scripts/Handlers/PreshPlayFabApiHandler.cs
+++ Assets/Scripts/Handlers/PreshPlayFabApiHandler.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 using PlayFab;
 using PlayFab.EconomyModels;
 using System;
-
+using Newtonsoft.Json;
 
 public class PreshPlayFabApiHandler : MonoBehaviour
 {
@@ -18,12 +18,13 @@ public class PreshPlayFabApiHandler : MonoBehaviour
         AutoLogin();
     }
     //Login
-    void AutoLogin() 
-    { 
-        var request =new LoginWithCustomIDRequest{
+    void AutoLogin()
+    {
+        var request = new LoginWithCustomIDRequest
+        {
             CreateAccount = true,
             CustomId = "#req45",
-            TitleId= "10F24",
+            TitleId = "10F24",
         };
 
         PlayFabClientAPI.LoginWithCustomID(request, onLoginSuccess, onError);
@@ -31,8 +32,9 @@ public class PreshPlayFabApiHandler : MonoBehaviour
 
     private void onLoginSuccess(LoginResult obj)
     {
-        print("user Auth success");
-        Invoke(nameof(AddInventoryItem), 5);
+        // print("user Auth success");
+        // Invoke(nameof(AddInventoryItem), 1);
+        Invoke(nameof(GetPlayerInventoryItems), 1);
     }
 
     //Adds Iventory Item
@@ -41,15 +43,15 @@ public class PreshPlayFabApiHandler : MonoBehaviour
         var request = new AddInventoryItemsRequest
         {
             Amount = 10,
-            Entity =new PlayFab.EconomyModels.EntityKey 
-            { 
-                Id = "362BF6719D3C0236",
-                Type= "title_player_account",
-            },
-            Item =new InventoryItemReference
+            Entity = new PlayFab.EconomyModels.EntityKey
             {
-                Id= "f986bbaa-260e-4542-b1f8-537852aefc6d",
-                StackId="default"
+                Id = "362BF6719D3C0236",
+                Type = "title_player_account",
+            },
+            Item = new InventoryItemReference
+            {
+                Id = "87203d93-c228-44b8-b81b-8d278cdcda9e",
+                StackId = "default"
             }
         };
         //print("working now");
@@ -64,11 +66,36 @@ public class PreshPlayFabApiHandler : MonoBehaviour
 
     private void OnInventoryError(PlayFabError error)
     {
-        print(error);
+        print(error.ErrorMessage);
     }
 
     private void onError(PlayFabError error)
     {
         Debug.Log(error.ErrorMessage);
+    }
+
+    void GetPlayerInventoryItems()
+    {
+        var request = new GetInventoryItemsRequest
+        {
+            CollectionId = "default",
+            Filter = "Type eq 'catalogItem'",
+            Entity = new PlayFab.EconomyModels.EntityKey
+            {
+                Id = "362BF6719D3C0236",
+                Type = "title_player_account",
+            }
+        };
+        PlayFabEconomyAPI.GetInventoryItems(request, OnGetInventorySuccess, onError);
+    }
+
+    private void OnGetInventorySuccess(GetInventoryItemsResponse result)
+    {
+        print(JsonConvert.SerializeObject(result));
+        for (int i = 0; i < result.Items.Count; i++)
+        {
+            var item = result.Items[i];
+            print(JsonConvert.SerializeObject(item.DisplayProperties));
+        }
     }
 }
