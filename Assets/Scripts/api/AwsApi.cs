be64028bb5824dbfbc3ec8b07d3253a4621434e5
NorVirae <norbertmbafrank@gmail.com>
2023-08-16 14:04:52 +0100

    finished get api call test

--- Assets/Scripts/api/AwsApi.cs
+++ Assets/Scripts/api/AwsApi.cs
@@ -1,3 +1,5 @@
+using Newtonsoft.Json;
+using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.Networking;
@@ -8,28 +10,26 @@ public class AwsApi : Singleton<AwsApi>
     // Start is called before the first frame update
     public IEnumerator RedeemCouponApiCall(string api)
     {
-        using(UnityWebRequest webRequest = new UnityWebRequest(api))
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(api))
         {
+            // Request and wait for the desired page.
             yield return webRequest.SendWebRequest();
 
+            string[] pages = api.Split('/');
+            int page = pages.Length - 1;
 
-            if (webRequest.result != UnityWebRequest.Result.Success)
+            switch (webRequest.result)
             {
-                Debug.LogError("Error: " + webRequest.error);
-
-            }
-            else
-            {
-                string responseText;
-                if (webRequest.downloadHandler != null)
-                {
-                    responseText = webRequest.downloadHandler.text;
-                    print(responseText);
-                }
-                else
-                {
-                    Debug.LogError("DownloadHandler is null.");
-                }
+                case UnityWebRequest.Result.ConnectionError:
+                case UnityWebRequest.Result.DataProcessingError:
+                    Debug.LogError(pages[page] + ": Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.ProtocolError:
+                    Debug.LogError(pages[page] + ": HTTP Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.Success:
+                    Debug.Log(pages[page] + ":\nReceived: " + JsonConvert.DeserializeObject(webRequest.downloadHandler.text));
+                    break;
             }
         }
     }
